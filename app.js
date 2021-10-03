53be915735571a48db707b0f886be8bdd60eb38e
AYAN ALI <73759379+Ayanal1@users.noreply.github.com>
2021-10-03 17:27:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,48 +1,41 @@
-console.log(firebase.database);
-var unorder_item = document.getElementById('unorder')
-var btuns = document.getElementById('btuns')
-function add_todo(){
-    var val = document.getElementById('inp')
+// const { default: firebase } = require("firebase")
+
+// console.log(firebase.database);
+// firebase.database().ref('/USER/').on('child_added',function(data){
+//     record = data.val()
+//     console.log(record.item)
 
 
+firebase.database().ref('/USER/').on('child_added',function(smit){
+    var data = smit.val()
+    console.log(data.todo)
 
-    if(val.value == ""){
-       alert("enter value")
-       
-    }
-    else{
-    
-    var lst_item = document.createElement('div')
-    var rowtext= document.createElement('h6')
-    var list_text = document.createTextNode(val.value)
-    // list_text.style.width = "20px"
-    lst_item.setAttribute('class',' list_item')
-    rowtext.setAttribute('class',' tabletext')
-    
-    rowtext.appendChild(list_text)
-    lst_item.appendChild(rowtext)
-    console.log(lst_item)
-    
-    // var obj = {
-        //     todo : val.value
-        // }
-        // console.log(obj.todo)
+
+        var lst_item = document.createElement('div')
+        var rowtext= document.createElement('h6')
+        var list_text = document.createTextNode(data.todo)
+        // list_text.style.width = "20px"
+        lst_item.setAttribute('class',' list_item')
+        rowtext.setAttribute('class',' tabletext')
         
-        // key= firebase.database().ref("/TODO_list").push(obj).getKey();
-        // console.log(key)
-        // var key = 13;
-        // firebase.database().ref(`/USER/`+key).set(obj)
+        rowtext.appendChild(list_text)
+        lst_item.appendChild(rowtext)
+        console.log(lst_item)
+
         
         var edit = document.createElement('button')
         var edit_text = document.createTextNode('')
         edit.setAttribute('onclick','edit(this)')
         edit.setAttribute('class','edit_btn fas fa-pencil-alt btn btn-primary')
-        
+        edit.setAttribute('id',data.key)
+
         var edit_2 = document.createElement('button')
         var edit_text_2 = document.createTextNode('')
         edit_2.setAttribute('onclick','delete_todo(this)')
         edit_2.setAttribute('class','edit_btn fas fa-trash-alt btn btn-danger')
+        edit_2.setAttribute('id',data.key)
         
+
         edit.appendChild(edit_text)
         edit_2.appendChild(edit_text_2)
 
@@ -61,9 +54,55 @@ function add_todo(){
         t4.appendChild(edit)
         t4.appendChild(edit_2)
         unorder_item.appendChild(t1)
-        val.value=''
-        
+        // val.value=''
 
+
+
+
+
+
+
+})
+
+
+var unorder_item = document.getElementById('unorder')
+var btuns = document.getElementById('btuns')
+
+
+
+function add_todo(){
+    var val = document.getElementById('inp')
+
+
+
+    if(val.value == ""){
+       alert("enter value")
+       
+    }
+    else{
+
+
+
+        NEW= firebase.database().ref().push().getKey();
+        console.log(NEW)
+        var obj = {
+                todo : val.value,
+                key : NEW, 
+            }
+            // console.log(obj.todo)
+            
+            // console.log(key)
+            // var key = 13;
+            firebase.database().ref(`/USER/`+NEW).set(obj)
+            
+            
+            
+            // firebase.database().ref('/USER/').once('value',function(data){
+            //     console.log(data.val())
+            // })
+    
+
+       
 
 }
 }
@@ -76,14 +115,22 @@ function edit(e){
         alert("enter value")
     }
     else{
-        e.parentNode.parentNode.childNodes[0].childNodes[0].innerText = inp
+        firebase.database().ref('/USER/').child(e.id).set({
+            todo:inp,
+            key:e.id})
+    }
+        // e.parentNode.parentNode.childNodes[0].childNodes[0].innerText = inp
     }
    
-}
+
 
 function delete_todo(e){
+console.log(e.id)
+firebase.database().ref("/USER/"+e.id).remove()
 e.parentNode.parentNode.remove()
+
 }
 function add_alltodo(){
+    firebase.database().ref("/USER/").remove()
     unorder_item.innerHTML=null
 }
