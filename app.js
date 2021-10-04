765d3c306e19c77466ad403e351c08509fa3fbc5
AYAN ALI <73759379+Ayanal1@users.noreply.github.com>
2021-10-03 23:09:46 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-// const { default: firebase } = require("firebase")
-
-// console.log(firebase.database);
-// firebase.database().ref('/USER/').on('child_added',function(data){
-//     record = data.val()
-//     console.log(record.item)
 
 
 firebase.database().ref('/USER/').on('child_added',function(smit){
     var data = smit.val()
-    console.log(data.todo)
+    
 
 
         var lst_item = document.createElement('div')
         var rowtext= document.createElement('h6')
         var list_text = document.createTextNode(data.todo)
-        // list_text.style.width = "20px"
-        lst_item.setAttribute('class',' list_item')
+       
+        // lst_item.setAttribute('class',' list_item')
         rowtext.setAttribute('class',' tabletext')
         
         rowtext.appendChild(list_text)
         lst_item.appendChild(rowtext)
-        console.log(lst_item)
+      
 
         
         var edit = document.createElement('button')
@@ -35,18 +29,20 @@ firebase.database().ref('/USER/').on('child_added',function(smit){
         edit_2.setAttribute('class','edit_btn fas fa-trash-alt btn btn-danger')
         edit_2.setAttribute('id',data.key)
         
-
+        
         edit.appendChild(edit_text)
         edit_2.appendChild(edit_text_2)
-
-        // lst_item.appendChild(edit)
-        // lst_item.appendChild(edit_2)
+        
+        
         
         t1=document.createElement('table')
         t2=document.createElement('tr')
         t3=document.createElement('td')
         t4=document.createElement('td')
-        // t3.setAttribute('class','tabletext')
+        t3.setAttribute('class','list_item')
+        t4.setAttribute('class','btns')
+        
+        
         t1.appendChild(t2)
         t2.appendChild(t3)
         t2.appendChild(t4)
@@ -84,22 +80,17 @@ function add_todo(){
 
 
         NEW= firebase.database().ref().push().getKey();
-        console.log(NEW)
+        
         var obj = {
                 todo : val.value,
                 key : NEW, 
             }
-            // console.log(obj.todo)
             
-            // console.log(key)
-            // var key = 13;
             firebase.database().ref(`/USER/`+NEW).set(obj)
             
             
             
-            // firebase.database().ref('/USER/').once('value',function(data){
-            //     console.log(data.val())
-            // })
+            
     val.value=''
 
        
@@ -109,7 +100,7 @@ function add_todo(){
 
 function edit(e){
     var val = e.parentNode.parentNode.childNodes[0].childNodes[0].innerText
-    console.log(val)
+   
     var inp = prompt("Enter Value",val)
     if(inp == ""){
         alert("enter value")
@@ -125,7 +116,7 @@ function edit(e){
 
 
 function delete_todo(e){
-console.log(e.id)
+
 firebase.database().ref("/USER/"+e.id).remove()
 e.parentNode.parentNode.remove()
 
